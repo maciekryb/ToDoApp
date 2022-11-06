9ccde26ced6baedf45e550ae10d26c65eab965a2
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-06 17:37:16 +0100

    Add Sorting options

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,6 +4,24 @@ import Task from './Task';
 const TaskList = (props) => {
   const active = props.tasks.filter((task) => task.active);
   const done = props.tasks.filter((task) => !task.active);
+
+  if(done.length >=2) {
+    done.sort((a, b) => {
+        return b.finishDate - a.finishDate;
+      });
+  }
+
+  if(active.length >=2) {
+
+    active.sort((a, b) => {
+     a=a.text.toUpperCase();
+     b=b.text.toUpperCase();
+      if(a < b) return -1
+      if(a > b) return 1
+      return 0
+      });
+  }
+
   const activeTasks = active.map((task) => (
     <Task
       key={task.id}
