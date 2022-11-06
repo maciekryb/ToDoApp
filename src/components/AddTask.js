7acb6a5b850f0cb6713cc92fe4cfd3a66b7110b8
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-06 16:00:50 +0100

    Program data choice

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './AddTask.css';
 
 class AddTask extends Component {
+  minDate = new Date().toISOString().slice(0, 10);
   state = {
     text: '',
     checked: false,
-    date: '2022-10-07',
+    date: this.minDate,
+  };
+
+  handleDate = (e) => {
+    this.setState({
+      date: e.target.value,
+    });
   };
 
   render() {
+    let maxDate = this.minDate.slice(0, 4) * 1 + 1;
+    console.log(maxDate);
+    maxDate = maxDate + '-01-01';
+    console.log(maxDate);
     return (
       <>
         <div className='form'>
@@ -23,9 +34,10 @@ class AddTask extends Component {
           <input
             type='date'
             value={this.state.date}
-            min='2022-10-01'
-            max='2023-01-01'
-          />{' '}
+            min={this.minDate}
+            max={maxDate}
+            onChange={this.handleDate}
+          />
           <br />
           <button>Dodaj</button>
         </div>
