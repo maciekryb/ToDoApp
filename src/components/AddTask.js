27f85295b000d9dfce15ce7e398ae2e9fe0865b4
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-06 13:42:37 +0100

    build form to add tasks

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import './AddTask.css';
 
 class AddTask extends Component {
-  state = {};
+  state = {
+    text: '',
+    checked: false,
+    date: '2022-10-07',
+  };
+
   render() {
     return (
       <>
-        <div>Dodaj task</div>
+        <div className='form'>
+          <input
+            type='text'
+            placeholder='dodaj zadanie'
+            value={this.state.text}
+          />
+          <input type='checkbox' checked={this.state.checked} id='important' />
+          <label htmlFor='important'>Priorytet</label> <br />
+          <label htmlFor='date'>Do kiedy zrobić </label>
+          <input
+            type='date'
+            value={this.state.date}
+            min='2022-10-01'
+            max='2023-01-01'
+          />{' '}
+          <br />
+          <button>Dodaj</button>
+        </div>
         <hr />
       </>
     );
